54f8b8befc7affeb3d20728fccfe58d77e813ab4
Kenny Harsono <72096900+Kharsono@users.noreply.github.com>
2021-01-22 21:58:35 -0500

    Add files via upload

---
+++ hw1_2020fall/Tests/42/visual_recog 42.py
@@ -0,0 +1,319 @@
+import os, math, multiprocessing
+from os.path import join
+from copy import copy
+
+import numpy as np
+from PIL import Image
+
+import visual_words
+
+
+def get_feature_from_wordmap(opts, wordmap):
+    '''
+    Compute histogram of visual words.
+
+    [input]
+    * opts      : options
+    * wordmap   : numpy.ndarray of shape (H,W)
+
+    [output]
+    * hist: numpy.ndarray of shape (K)
+    '''
+
+    K = opts.K  #K is number of words
+    # ----- TODO -----
+
+    #Calculate occurences
+    hist, bin_e = np.histogram(wordmap, K, density = False)
+    bin_w = bin_e[2] - bin_e[1]
+
+
+    #Check sum of count elements -> should be # of pixels
+    pix = wordmap.shape[0] * wordmap.shape[1]
+    nhist = hist/pix
+
+    '''
+    #Plotting
+    import matplotlib.pyplot as plt
+
+    plt.figure()
+    plt.bar(bin_e[:-1], nhist, label = "heights nhist")
+    plt.xlabel("Index Dict")
+    plt.ylabel("Percentage of showing up")
+    plt.grid()
+    plt.legend()
+    plt.title("Test Hist")
+    plt.show()
+    '''
+
+    return nhist
+    pass
+
+
+def get_feature_from_wordmap_SPM(opts, wordmap):
+    '''
+    Compute histogram of visual words using spatial pyramid matching.
+
+    [input]
+    * opts      : options
+    * wordmap   : numpy.ndarray of shape (H,W)
+
+    [output]
+    * hist_all: numpy.ndarray of shape (K*(4^L-1)/3)
+    '''
+        
+    K = opts.K #Number of histog columns
+    L = opts.L #Number of layers
+    # ----- TODO -----
+    
+    # Initialization weightage of layer
+    wght = np.empty(L)
+    '''
+    for x in range(L):
+        if (x == 0 or x == 1):
+            wght[x] = 1/(pow(2, L))
+        else:
+            wght[x] = pow(2, (x-L))
+    '''
+
+    # Calculate number of smaller boxes
+    size = int(K*(pow(4,L)-1)/3)
+    histall = np.empty([size]) #contains all histogram of all layers
+    for x in range(L, 0, -1): #iterate layers going from most fine
+        if (L == 1 or L == 2):
+            wght[x-1] = (1/pow(2,(L-1)))
+        else:
+            wght[x-1] = pow(2,(x-L-1))
+            if (x == 1):
+                wght[x-1] = wght[x+1]
+        szb = pow(2, (x-1))
+        leng = int(wordmap.shape[0]/szb)
+        hei = int(wordmap.shape[1]/szb)
+        chunk = np.empty(K)             #this is how many boxes & keeps szb*szb amount of histograms
+        content = np.empty([leng, hei]) #attempt on copying image in each splitted box
+        histmini = np.empty(szb*szb*K)  #result of first concatenate aka per layer
+        for a in range(szb):
+            for b in range(szb):        #a & b identify which chunk is it in
+                for y in range(leng):       #do this for # column IN each chunk/box
+                    for z in range(hei):    #do this for # row IN each chunk/box
+                        content[y,z] = wordmap[(a*leng + y), (b*hei + z)]
+                chunk = get_feature_from_wordmap(opts, content)     #has each histogram
+
+                #1st concatenating within each layer
+                if a==0 and b==0 :
+                    histmini = chunk  
+                else:
+                    histmini = np.concatenate((histmini, chunk), axis = None)    
+                
+
+        #2nd concatenate + adding weighting scheme
+        if(x == L):
+            histall = histmini * wght[x-1]
+        else:
+            histall = np.concatenate((histall, (histmini * wght[x-1])), axis = None)
+
+    #print(L, histall.shape)
+        
+    return histall
+    pass
+
+
+    
+def get_image_feature(opts, img_path, dictionary):
+    '''
+    Extracts the spatial pyramid matching feature.
+
+    [input]
+    * opts      : options
+    * img_path  : path of image file to read
+    * dictionary: numpy.ndarray of shape (K, 3F)
+
+
+    [output]
+    * feature: numpy.ndarray of shape (K)
+    '''
+
+    # ----- TODO -----
+    print(" get feat called")
+    img = Image.open(img_path)      #uploading image and convert into array
+    img = np.array(img).astype(np.float32)/255
+    wordmap = visual_words.get_visual_words(opts, img, dictionary)  #grab the wordmap for each image
+    feat = get_feature_from_wordmap_SPM(opts, wordmap)  #convert the wordmap into histogram with super long bins (depending on the L)
+    #print(opts.L, feat.shape)
+    
+    return feat
+    
+    pass
+
+def build_recognition_system(opts, n_worker=1):
+    '''
+    Creates a trained recognition system by generating training features from all training images.
+
+    [input]
+    * opts        : options
+    * n_worker  : number of workers to process in parallel
+
+    [saved]
+    * features: numpy.ndarray of shape (N,M)
+    * labels: numpy.ndarray of shape (N)
+    * dictionary: numpy.ndarray of shape (K,3F)
+    * SPM_layer_num: number of spatial pyramid layers
+    '''
+
+    data_dir = opts.data_dir
+    out_dir = opts.out_dir
+    SPM_layer_num = opts.L
+
+    train_files = open(join(data_dir, 'train_files.txt')).read().splitlines()
+    train_labels = np.loadtxt(join(data_dir, 'train_labels.txt'), np.int32)
+    dictionary = np.load(join(out_dir, 'dictionary.npy'))
+
+    # ----- TODO -----
+
+    #import pdb; pdb.set_trace()
+    
+    #Initializing. Purpose: Find features and labels
+    K = opts.K; L = opts.L
+    size = int(K*(pow(4,L)-1)/3)
+    T = len(train_files)
+    feat = np.empty([T,size]); lab = np.empty(T)
+
+    #Finding features & labels for T number files
+    for x in range(T):
+        print(x)
+        path = join(opts.data_dir, train_files[x])
+        feat[x,:] = get_image_feature(opts, path, dictionary)
+        lab[x] = train_labels[x]
+
+    np.savez_compressed(join(out_dir, 'trained_system.npz'),
+        features=feat,
+        labels=lab,
+        dictionary=dictionary,
+        SPM_layer_num=SPM_layer_num,
+    )
+    
+    pass
+
+
+
+    ## example code snippet to save the learned system
+    # np.savez_compressed(join(out_dir, 'trained_system.npz'),
+    #     features=features,
+    #     labels=train_labels,
+    #     dictionary=dictionary,
+    #     SPM_layer_num=SPM_layer_num,
+    # )
+
+def distance_to_set(word_hist, histograms):
+    '''
+    Compute similarity between a histogram of visual words with all training image histograms.
+
+    [input]
+    * word_hist: numpy.ndarray of shape (K)
+    * histograms: numpy.ndarray of shape (N,K)
+
+    [output]
+    * sim: numpy.ndarray of shape (N)
+    '''
+
+    # ----- TODO -----
+    
+    sim = np.empty(histograms.shape[0])
+    for i in range(histograms.shape[0]):                # Number of T or N => Numb of features
+        score = np.minimum(word_hist, histograms[i])    # find all the min values and array it
+        total = 0
+        for j in range(histograms.shape[1]): # Number of bins or minimum vals to add to
+            total = total + score[j]         # summing all the min bin values
+        sim[i] = total                       # Most minim value should be 1 and most max val should be 2 (we normalized)
+    sim = 1 - sim
+    return sim
+    pass    
+
+    #broadcast array function -> check this out
+    #duplicate and directly see instead of for loop
+    
+def evaluate_recognition_system(opts, n_worker=1):
+    '''
+    Evaluates the recognition system for all test images and returns the confusion matrix.
+
+    [input]
+    * opts        : options
+    * n_worker  : number of workers to process in parallel
+
+    [output]
+    * conf: numpy.ndarray of shape (8,8)
+    * accuracy: accuracy of the evaluated system
+    '''
+
+    data_dir = opts.data_dir
+    out_dir = opts.out_dir
+
+    trained_system = np.load(join(out_dir, 'trained_system.npz'))
+    dictionary = trained_system['dictionary']
+
+    # using the stored options in the trained system instead of opts.py
+    test_opts = copy(opts)
+    test_opts.K = dictionary.shape[0]
+    test_opts.L = trained_system['SPM_layer_num']
+
+    test_files = open(join(data_dir, 'test_files.txt')).read().splitlines()
+    test_labels = np.loadtxt(join(data_dir, 'test_labels.txt'), np.int32)
+
+    # ----- TODO -----
+
+    # Initializing variables    
+    N = len(test_files)
+    histograms = trained_system['features']
+    trained_labels = trained_system['labels']
+    size = int(test_opts.K*(pow(4,opts.L)-1)/3)
+    feat = np.empty([N, size])  # features for each image in test files
+    minloc = 0                  # index for finding label in trained files
+    calc_labels = np.empty(N)   # labels for test_files (program)
+
+    # Compare test labels from program and true value
+    accuracy = 0
+    conf = np.empty([8,8])
+    for i in range(8):
+        for j in range(8):
+            conf[i,j] = 0
+    
+    # Finding the labels for the test_files through program
+    mix = None
+    for i in range(N):
+        print(i)
+        path = join(opts.data_dir, test_files[i])
+        feat[i,:] = get_image_feature(test_opts, path, dictionary)
+        dist = distance_to_set(feat[i], histograms)
+        minloc = np.argmin(dist)                    #find the index of lowest distance => most similar
+        calc_labels[i] = trained_labels[minloc]     #find labels from program
+        conf[ int(test_labels[i]) , int(calc_labels[i]) ] += 1
+        if (int(test_labels[i]) == 4 and int(calc_labels[i]) == 3):
+            print(path, test_labels[i], calc_labels[i])
+             
+    accuracy = np.trace(conf)/N
+
+    print(calc_labels)
+    return conf, accuracy
+            
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hw1_2020fall/Tests/original 42/visual_words.py
@@ -0,0 +1,216 @@
+import os, multiprocessing
+from os.path import join, isfile
+
+import numpy as np
+from PIL import Image
+import scipy.ndimage
+import skimage.color
+#import tempfile
+#import random
+#from sklearn.cluster import KMeans
+
+
+def extract_filter_responses(opts, img):
+    '''
+    Extracts the filter responses for the given image.
+
+    [input]
+    * opts    : options
+    * img    : numpy.ndarray of shape (H,W) or (H,W,3)
+    [output]
+    * filter_responses: numpy.ndarray of shape (H,W,3F)
+    '''
+    
+    filter_scales = opts.filter_scales
+    # ----- TODO -----
+
+    # Check if image is floating point
+    
+    # Check if image has 3 channels
+    if img.ndim == 2:
+        img = np.array([img, img, img])
+
+    if img.shape[0] == 3:
+        first=img.shape[0]; second=img.shape[1]; third=img.shape[2]
+        img = np.reshape(img, (second,third,first))
+    
+    # Convert to Lab color space & initialize final array
+    img = skimage.color.rgb2lab(img)
+    filter_responses = np.empty((np.size(img,0), np.size(img,1), 0)) #check filled. Align with utility
+
+    # Apply filter bank  
+    for z in range(len(filter_scales)): #run all filters first before moving on to next scale
+        temp = np.empty_like (img)
+        for x in range(4): #run for 4 image filters
+            for y in range(3): #run for 3 rgb channels (separated)
+                if x == 0:
+                    temp[:,:,y] = scipy.ndimage.gaussian_filter(img[:,:,y], filter_scales[z])
+                if x == 1:
+                    temp[:,:,y] = scipy.ndimage.gaussian_laplace(img[:,:,y], filter_scales[z])
+                if x == 2:
+                    temp[:,:,y] = scipy.ndimage.gaussian_filter(img[:,:,y], filter_scales[z], order=[0,1]) #gaussian x-dir
+                if x == 3:
+                    temp[:,:,y] = scipy.ndimage.gaussian_filter(img[:,:,y], filter_scales[z], order=[1,0]) #gaussian y-dir
+            comb = np.dstack((temp[:,:,0],temp[:,:,1],temp[:,:,2])) #combining the separated 3 rgb channels
+            filter_responses = np.concatenate((filter_responses, comb),2) #combining filter_responses for the whole image
+    
+    return filter_responses
+
+    pass
+
+
+def compute_dictionary_one_image(args):
+    '''
+    Extracts a random subset of filter responses of an image and save it to disk
+    This is a worker function called by compute_dictionary
+
+    Your are free to make your own interface based on how you implement compute_dictionary
+    highway/sun_bmjizcavvsculkfz.jpg
+    park/sun_avnbrpzxesubtazc.jpg
+    windmill/sun_baueqhsqpjpjuhfz.jpg
+
+    '''
+
+    # ----- TODO -----
+    import random
+
+    print("\nFUNCTION CALLED")
+    al = 25
+    filter_responses = np.empty((0, args.shape[2]))
+    
+    ##args is array of the current image file
+    print(args.shape)
+    
+    for i in range(al): # 25 random pixels
+        h = random.randrange(0, np.size(args,0), 1); v = random.randrange(0, np.size(args,1), 1)
+        #print("third for loop...random pixels")
+        #print(i, h, v)
+        #print(comb[h,v])
+        #print(d.read())
+        filter_responses = np.append(filter_responses, [args[h,v]], axis=0)
+        #print(comb[h,v])
+
+    print("Final print\n")
+     
+    return filter_responses
+    pass
+
+
+def compute_dictionary(opts, n_worker=1):
+    '''
+    Creates the dictionary of visual words by clustering using k-means.
+
+    [input]
+    * opts         : options
+    * n_worker     : number of workers to process in parallel
+    
+    [saved]
+    * dictionary : numpy.ndarray of shape (K,3F)
+    '''
+
+    data_dir = opts.data_dir
+    feat_dir = opts.feat_dir
+    out_dir = opts.out_dir
+    K = opts.K
+
+    train_files = open(join(data_dir, 'train_files.txt')).read().splitlines()
+    # ----- TODO -----
+
+    # Initialization
+    import sklearn.cluster
+    T = len(train_files)
+    #print(train_files); print(T)
+
+    # Find all the filter_reponses by calling compute_dict_one for every image
+    for x in range(T):
+        path = join(opts.data_dir, train_files[x])
+        print(path)
+        img = Image.open(path)
+        img = np.array(img).astype(np.float32)/255
+        img = extract_filter_responses(opts, img) # become: (x,y, 3*F*filter_scales)
+        filter_responses = compute_dictionary_one_image(img)
+        filter_responses = np.append(filter_responses, filter_responses, axis=0)
+    #d.seek(0)
+
+    # Compute kmeans & clusters, then export dictionary file
+    print(filter_responses.shape)
+    kmeans = sklearn.cluster.KMeans(n_clusters=K).fit(filter_responses)
+    dictionary = kmeans.cluster_centers_
+    np.save('dictionary.npy', dictionary)
+    
+    pass
+
+    ## example code snippet to save the dictionary
+    # np.save(join(out_dir, 'dictionary.npy'), dictionary)
+
+def get_visual_words(opts, img, dictionary):
+    '''
+    Compute visual words mapping for the given img using the dictionary of visual words.
+
+    [input]
+    * opts    : options
+    * img    : numpy.ndarray of shape (H,W) or (H,W,3)
+    
+    [output]
+    * wordmap: numpy.ndarray of shape (H,W)
+    '''
+    
+    # ----- TODO -----
+
+    import scipy.spatial #conda doesn't show in my computer (although installed) -> default to python 3.8 and it requires import
+
+    img = extract_filter_responses(opts, img) # become: (x,y, 3*F(4)*filter_scales)
+
+    '''
+    # For loops for each column, row & K words to compare the distance
+    wordmap = np.empty([img.shape[0], img.shape[1]])
+    product = img.shape[0] * img.shape[1]
+    img = np.reshape(img, (-1,3))
+    diff = np.empty([product, dictionary.shape[0]])
+    diff = scipy.spatial.distance.cdist(img, dictionary)
+
+    for x in range(product):
+        wordmap[x] = np.argmin(diff[x,:])
+
+    wordmap = np.reshape(wordmap, (img.shape[0], img.shape[1]), 'C')
+    '''
+    
+    # For loops for each column, row & K words to compare the distance
+    wordmap = np.empty([img.shape[0], img.shape[1]])
+    for x in range(img.shape[0]): #repeat all for the number of columns
+        diff = scipy.spatial.distance.cdist(img[x,:,:], dictionary) #only accepts 2d matrix so compare 1 whole row with 10 k words (all in rgb -> 3 components)
+        for y in range(img.shape[1]): #find the min distance for each row
+            wordmap[x,y] = 0 #wordmap is just indexing of dictionary
+            temp = diff[y,0]
+            for z in range(dictionary.shape[0]): #there's 10 distance differences to compare
+                if (diff[y,z] < temp):#comparing 1 < 0 till 10 < 9 ==> 9 steps
+                    wordmap[x,y] = z
+                    temp = diff[y,z]
+    
+    
+    #import pdb; pdb.set_trace()
+    return wordmap
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
